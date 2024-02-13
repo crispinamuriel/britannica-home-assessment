c441ab902c813b2e693919406d6afb796611ce44
crispinamuriel <crispinamuriel@gmail.com>
2024-02-13 17:06:49 -0500

    handle profile error

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Profile } from '../../pages/types/Profile';
+import { notFound } from 'next/navigation';
 import Link from 'next/link';
 
 interface DynamicRouteProps {
@@ -17,6 +18,10 @@ export default async function ProfileShowPage(props: DynamicRouteProps) {
 
   const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
   const profile: Profile = await response.json();
+
+  if (!profile) {
+    return notFound();
+  }
 
   return (
     <div className="flex flex-row max-w-sm w-full lg:max-w-full lg:flex m-10 max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-70 hover:bg-slate-950">
